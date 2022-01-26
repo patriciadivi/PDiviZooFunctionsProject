d42f01abca175267164e24086b9116a2d9d6d0bd
PatriciaDivi <patriciavianad@gmail.com>
2022-01-26 16:25:24 -0300

    1 Requisito |  Crie uma função que busca as espécies de animais por id | concluido | :rocket:

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,14 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+function getSpeciesByIds(...ids) {
   // seu código aqui
+  // const { species } = data;
+  return species.filter(({ id }) => ids.find((elementId) => elementId === id));
 }
+
+// console.log(species.filter(({ id }) => id))
+// console.log(species.find((value) => value.id));
+// console.log(species.filter((fnCall) => fnCall.id).map((mapCall) => mapCall.id));
 
 module.exports = getSpeciesByIds;
