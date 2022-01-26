ba40ab8038e1dddb25c947014d17a50da587ffad
PatriciaDivi <patriciavianad@gmail.com>
2022-01-26 20:37:11 -0300

    2 Requisito | Implemente a função getAnimalsOlderThan | CONCLUIDO | :rocket:

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
-function getAnimalsOlderThan(animal, age) {
+function getAnimalsOlderThan(animal, ageMin) {
   // seu código aqui
+  const { species } = data;
+
+  return species.find(({ name }) => name === animal).residents.every(({ age }) => age >= ageMin);
 }
+
+// console.log(species.filter(({ name }) => name));
+// console.log(getAnimalsOlderThan('otters', 7));
+// console.log(species.find(({ name }) => name).residents);
+// age.find((elementId) => elementId >= age))
 
 module.exports = getAnimalsOlderThan;
