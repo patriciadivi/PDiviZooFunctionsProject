a843a5e7d502de3dab4dbef9de901744d81ccae9
PatriciaDivi <patriciavianad@gmail.com>
2022-01-27 17:04:13 -0300

    4 Requisito | Crie a gestão dos funcionários de acordo com o respectivo gerente | Concluido | :rocket:

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,19 @@
 const data = require('../data/zoo_data');
+const { employees } = data;
 
 function isManager(id) {
   // seu código aqui
+  return employees.some(({ managers }) => managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
   // seu código aqui
+  if (!isManager(managerId)) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  const filterManagerId = employees.filter(({ managers }) => managers.includes(managerId))
+    .map(({ firstName, lastName }) => `${firstName} ${lastName}`);
+  return filterManagerId;
 }
 
 module.exports = { isManager, getRelatedEmployees };
