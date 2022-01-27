35585e143c470984fdbc5522ec2696f62fd8c4cc
PatriciaDivi <patriciavianad@gmail.com>
2022-01-27 13:00:34 -0300

    3 Requisito | Implemente a função getEmployeeByName| CONCLUIDO| :rocket:

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -2,6 +2,15 @@ const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  const { employees } = data;
+  if (!employeeName) return {};
+  const findEmployeeName = employees.find(({ firstName, lastName }) =>
+    firstName === employeeName || lastName === employeeName);
+  return findEmployeeName;
 }
+
+// console.log(employees.filter(({ firstName }) => firstName));
+// console.log(employees.find(({ firstName }) => firstName));
+// console.log(employees.find(({ lastName }) => lastName));
 
 module.exports = getEmployeeByName;
