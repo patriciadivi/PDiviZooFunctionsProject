432de1128a3599408475fa11df797a1e110e9e6c
PatriciaDivi <patriciavianad@gmail.com>
2022-01-29 04:13:05 -0300

    6 Requisito | Implemente a função calculateEntry | CONCLUIDO | :rocket:

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,12 +1,16 @@
-const data = require("../data/zoo_data");
+const data = require('../data/zoo_data');
+
 const { prices } = data;
+
 function countEntrants(entrants) {
   // REF https://github.com/tryber/sd-19-c-project-zoo-functions/blob/otto-micheletti-project-zoo-functions/src/calculateEntry.js
   const filterChild = entrants.filter((childAge) => childAge.age < 18).length;
   const filterAdult = entrants.filter(
-    (adultAge) => adultAge.age >= 18 && adultAge.age < 50).length;
+    (adultAge) => adultAge.age >= 18 && adultAge.age < 50,
+  ).length;
   const filterSenior = entrants.filter(
-    (seniorAge) => seniorAge.age >= 50).length;
+    (seniorAge) => seniorAge.age >= 50,
+  ).length;
   return { adult: filterAdult, child: filterChild, senior: filterSenior };
 }
 
